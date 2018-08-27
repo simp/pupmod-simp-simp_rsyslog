fad71129a3376699934228500f91c6fcde94bf8c
jeannegreulich <jeanne.greulich@onyxpoint.com>
2018-08-27 17:58:59 +0000

    (SIMP-5157) StreamDriverPermittedPeers  fixed   - added permitted_peers parameter so the stream_driver_permitted_peers     parameters of rsyslog could be set in the call to set up the     remote rule on the client.   - default permitted_peers to "LOGSERVERNAME" so the name in the     log_servers     array would be used.   - updated metadata.json to require pupmod-simp-rsyslog 7.2.0 or later     so the default value "LOGSERVERNAME" would work.   - The above changes will allow the client to send to remote log     servers that are in a different domain then itself.  When using TLS     the FQDN of the remote logserver should be used in the log_server     array.  If it is not used, be sure to set the permitted_peers     parameter in hiera.
    SIMP-5157 #close

--- manifests/forward.pp
+++ manifests/forward.pp
@@ -21,12 +21,26 @@
 #     systems where you want a minimum of log information to be captured
 #     locally.
 #
+# @param permitted_peers
+#   If TLS is being used, StreamDriverPermittedPeers is used to check the certificates.
+#   permitted_peers is used to set this.  In general, certificates will use the FQDN
+#   of the host as the CN in the subject of the certificate.  The default, "LOGSERVERNAME"  assumes that
+#   $::simp_rsyslog::log_servers and  $::simp_rsyslog::failover_log_servers are a list
+#   of FQDNs and that certificates used by the log servers to encrypt traffic use this for the CN.
+#   If this is not the case, then you will need to set this variable appropriately.
+#   Example:  If IP Addresses are used in the two simp_rsyslog settings above and the CN in 
+#   the servers certs are server.my.domain and server2,my.other.domain: 
+#   Set this to "server.my.domain,server2.my.other.domain"
+#   @see https://www.rsyslog.com/doc/v8-stable/configuration/modules/omfwd.html
+#   for more information on how to set this.
+#
 # @author Trevor Vaughan <tvaughan@onyxpoint.com>
 #
 class simp_rsyslog::forward (
-  Integer                  $order           = 99,
-  Enum['tcp','udp','relp'] $dest_type       = 'tcp',
-  Boolean                  $stop_processing = false
+  Integer                  $order            = 99,
+  Enum['tcp','udp','relp'] $dest_type        = 'tcp',
+  Boolean                  $stop_processing  = false,
+  String                   $permitted_peers  = 'LOGSERVERNAME'
 ){
   assert_private()
 
@@ -40,10 +54,11 @@ class simp_rsyslog::forward (
 
 
   rsyslog::rule::remote { "${order}_simp_rsyslog_profile_remote":
-    rule                 => $::simp_rsyslog::security_relevant_logs,
-    dest                 => $::simp_rsyslog::log_servers,
-    failover_log_servers => $::simp_rsyslog::failover_log_servers,
-    dest_type            => $dest_type,
-    stop_processing      => $stop_processing
+    rule                          => $::simp_rsyslog::security_relevant_logs,
+    dest                          => $::simp_rsyslog::log_servers,
+    failover_log_servers          => $::simp_rsyslog::failover_log_servers,
+    dest_type                     => $dest_type,
+    stream_driver_permitted_peers => $permitted_peers,
+    stop_processing               => $stop_processing
   }
 }
