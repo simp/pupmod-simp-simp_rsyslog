4283d242e94f28ebe4ef74a4d0a8b52044e32d25
Chris Tessmer <chris.tessmer@onyxpoint.com>
2025-06-10 22:10:44 -0400

    Fix rubocop complaints

--- spec/functions/simp_rsyslog/merge_hash_of_arrays_spec.rb
+++ spec/functions/simp_rsyslog/merge_hash_of_arrays_spec.rb
@@ -2,29 +2,31 @@
 require 'spec_helper'
 
 describe 'simp_rsyslog::merge_hash_of_arrays' do
+  # rubocop:disable RSpec/IndexedLet
   let(:hash1) do
     {
       'programs'   => ['program1', 'program2'],
-    'facilities' => ['facility1'],
-    'msg_starts' => ['start1', 'start2']
+      'facilities' => ['facility1'],
+      'msg_starts' => ['start1', 'start2'],
     }
   end
 
   let(:hash2) do
     {
       'programs'   => ['program2', 'program3'],
-    'msg_starts' => ['start0']
+      'msg_starts' => ['start0'],
     }
   end
 
   let(:hash3) do
     {
       'programs'   => ['program1', 'program4', 'program0'],
-    'facilities' => ['facility3'],
-    'msg_starts' => [],
-    'msg_regex'  => ['msg[0-9]+']
+      'facilities' => ['facility3'],
+      'msg_starts' => [],
+      'msg_regex'  => ['msg[0-9]+'],
     }
   end
+  # rubocop:enable RSpec/IndexedLet
 
   context 'with valid parameters' do
     it { is_expected.to run.with_params(hash1, {}).and_return(hash1) }
@@ -35,7 +37,7 @@ describe 'simp_rsyslog::merge_hash_of_arrays' do
           'programs'   => ['program1', 'program2', 'program3', 'program4', 'program0'],
           'facilities' => ['facility1', 'facility3'],
           'msg_starts' => ['start1', 'start2', 'start0'],
-          'msg_regex'  => ['msg[0-9]+']
+          'msg_regex'  => ['msg[0-9]+'],
         },
       )
     }
